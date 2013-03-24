76df59423b2ea3d6ba525433b41050a49cf4bd0d
Adam Turtle <themightybun@gmail.com>
2013-03-24 13:54:53 +0000

    First upload

---
+++ application/modules/templates/controllers/templates.php
@@ -0,0 +1,8 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Templates extends MY_Controller {
+    
+    public function home(){
+        //create templates here
+    }
+}
