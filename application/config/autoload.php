47c21a6e2d8d5e14ad2d80c10cf5de3cab74e968
Adam Turtle <themightybun@gmail.com>
2013-03-25 09:00:06 +0000

    Added HTML purifier
    updated autoload for html purifier, added helper and files into
    third_parties

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -64,7 +64,7 @@ $autoload['libraries'] = array('session','database');
 |	$autoload['helper'] = array('url', 'file');
 */
 
-$autoload['helper'] = array('url','file','form');
+$autoload['helper'] = array('url','file','form','htmlpurifier');
 
 
 /*
